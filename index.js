e6d5ea3987e5f01d50c208b4a1f6adda4e648e35
levideutsch <levideutsch@live.com>
2022-09-18 21:06:19 -0400

    commit tree 1c0e6a484c11ec8f78c496f108f2f3dbab037b7f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+// function  findMatching(arrrayOfDrivers, string) {
+//     for (const string of arrrayOfDrivers) {
+//         if (arrrayOfDrivers.string === string) {
+//             return driver.string
+//         }
+//     }
+// }
+
+function findMatching(arrrayOfDrivers, string) {
+    return arrrayOfDrivers.filter(function(driver){
+        if (driver.toUpperCase() === string.toUpperCase()) {
+        return string
+    }
+    }) 
+}
+
+function fuzzyMatch(arrrayOfDrivers2, string2) {
+    return arrrayOfDrivers2.filter(function(driver2) {
+        if (driver2[0] === string2[0]) {
+            return driver2
+        }
+    })
+}
+
+function matchName(arrrayOfDrivers3, string3) {
+    return arrrayOfDrivers3.filter(function(driver3) {
+        if (driver3.name === string3) {
+            return driver3.name 
+        }
+    })
+}
